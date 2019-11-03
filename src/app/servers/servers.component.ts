7a98bad128681067b21da40a07889f8f27037319
Dev Baweja <18ucs081@lnmiit.ac.in>
2019-11-04 01:00:32 +0530

    Two Way Binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -18,7 +18,7 @@ export class ServersComponent implements OnInit {
 
   allowNewServer : boolean = false;
   serverCreationStatus : string = 'No server was created!';
-  serverName : string = '';
+  serverName : string = 'Test';
 
   constructor() { 
     setTimeout( () => {
